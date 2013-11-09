728378f3b86f6f528eed68fde5f59d713126959c
eternaldensity <ed@chirpingmustard.com>
2013-11-10 05:11:04 +1100

    furnace multitasking description

--- castle.js
+++ castle.js
@@ -205,7 +205,7 @@ Molpy.Up=function()
 		++++++++++++++++++++++++++++++++++*/
 		Molpy.Life=0; //number of gameticks that have passed
 		Molpy.fps = 30 //this is just for paint, not updates
-		Molpy.version=2.93;
+		Molpy.version=2.94;
 		
 		Molpy.time=new Date().getTime();
 		Molpy.newpixNumber=1; //to track which background to load, and other effects...
