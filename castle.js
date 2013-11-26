09917460eaae73d07cf99a75932385f30566186e
Richard Proctor <richard@waveney.org>
2013-11-26 12:52:25 +0000

    Issue #281

--- castle.js
+++ castle.js
@@ -3732,7 +3732,7 @@ Molpy.Up=function()
 				}else{
 					gift=Math.ceil(gift/100);
 					Molpy.AddChips(gift);				
-					if(gift>1&&MMolpy.Boosts['AA'].power) Molpy.Notify(Molpify(gift,3)+' Glass Chips from '+Molpy.Boosts['SGC'].name,1);
+					if(gift>1&&Molpy.Boosts['AA'].power) Molpy.Notify(Molpify(gift,3)+' Glass Chips from '+Molpy.Boosts['SGC'].name,1);
 				}
 			}
 		}
