11a26e0fd0e4d2eb8dedcc952aabb7a39f3fff5d
eqbot <trentkenn8@gmail.com>
2014-02-05 17:00:51 -0800

    Stopped a mustard when 0 sand is spent.

--- gui.js
+++ gui.js
@@ -1512,7 +1512,7 @@ Molpy.DefineGUI = function() {
 		g('manualsandstat').innerHTML = Molpify(Molpy.sandManual, 4);
 		g('clicksstat').innerHTML = Molpify(Molpy.beachClicks, 4);
 		g('spclickstat').innerHTML = Molpify(Molpy.computedSandPerClick, 4);
-		g('sandspentstat').innerHTML = Molpify(Molpy.Boosts['Sand'].spent, 4);
+		g('sandspentstat').innerHTML = Molpify((Molpy.Boosts['Sand'].spent == undefined) ? 0 : Molpy.Boosts['Sand'].spent, 4);
 		g('totalcastlesstat').innerHTML = Molpify(Molpy.Boosts['Castles'].bought, 4);
 		g('destroyedcastlesstat').innerHTML = Molpify(Molpy.castlesDestroyed, 4);
 		g('downcastlesstat').innerHTML = Molpify(Molpy.totalCastlesDown, 4);
