66d0c6da2dd130c5292115a805a40d9e5a63d343
eternaldensity <ed@chirpingmustard.com>
2014-02-14 14:23:31 +1100

    redo

--- boosts.js
+++ boosts.js
@@ -4868,6 +4868,7 @@ Molpy.DefineBoosts = function() {
 			Molpy.DoMouldWork(left);
 		}
 
+		Molpy.boostSilence++;
 		var furn = Math.floor((times + Math.random() * 3) / 2);
 		if(Molpy.Got('Stretchable Chip Storage'))
 			Molpy.RewardBlastFurnace(furn);
@@ -4876,7 +4877,6 @@ Molpy.DefineBoosts = function() {
 				Molpy.RewardBlastFurnace();
 		}
 		left = times - furn;
-		Molpy.boostSilence++;
 		if(left > 7 && Molpy.Got('Milo')) {
 			var mr = Molpy.Boosts['Milo'];
 			var s = 0;// Math.sin((Math.PI*Molpy.ONGelapsed)/(Molpy.NPlength*100));
@@ -4950,18 +4950,14 @@ Molpy.DefineBoosts = function() {
 	Molpy.MakeGlassCeiling(10);
 	Molpy.MakeGlassCeiling(11);
 	Molpy.Boosts['Glass Ceiling 10'].price = {
-		price:{
-			Sand: '6FQ',
-			Castles: '6FQ',
-			GlassBlocks: '100K'
-		},
+		Sand: '6FQ',
+		Castles: '6FQ',
+		GlassBlocks: '100K'
 	};
 	Molpy.Boosts['Glass Ceiling 11'].price = {
-		price:{
-			Sand: '6WQ',
-			Castles: '6WQ',
-			GlassBlocks: '350K'
-		},
+		Sand: '6WQ',
+		Castles: '6WQ',
+		GlassBlocks: '350K'
 	};
 
 	new Molpy.Boost({
@@ -5197,16 +5193,18 @@ Molpy.DefineBoosts = function() {
 			str += 'Single use: available again when you have ' + Molpy.PriceString(Molpy.CalcRushCost(1, 1)) + '.'
 				+ (me.Level ? '<br>Currently at ' + Molpify(me.Level / 2, 1) + ' points' : '');
 			if (me.bought) {
-				str += '<br>';
-				if (!rushcost.Vacuum) {
-					str += '<input type="Button" onclick="Molpy.PantherRush()" value="Use"></input>';
-				} else {
-					if (Molpy.Has('Vacuum',rushcost.Vacuum)) str +=
-						'<input type="Button" onclick="Molpy.PantherRush()" value="Use Vacuums"></input>';
-					if (Molpy.Has('Mustard',rushcost.Vacuum)) str +=
-						'<input type="Button" onclick="Molpy.PantherRush(1)" value="Use Mustard"></input>';
-					if (Molpy.Has('Bonemeal',rushcost.Vacuum*10)) str +=
-						'<input type="Button" onclick="Molpy.PantherRush(2)" value="Use Bonemeal"></input>';
+				if (Molpy.Has('Blackprints',rushcost.Blackprints)) {
+					str += '<br>';
+					if (!rushcost.Vacuum) {
+						str += '<input type="Button" onclick="Molpy.PantherRush()" value="Use"></input>';
+					} else {
+						if (Molpy.Has('Vacuum',rushcost.Vacuum)) str +=
+							'<input type="Button" onclick="Molpy.PantherRush()" value="Use Vacuums"></input>';
+						if (Molpy.Has('Mustard',rushcost.Vacuum)) str +=
+							'<input type="Button" onclick="Molpy.PantherRush(1)" value="Use Mustard"></input>';
+						if (Molpy.Has('Bonemeal',rushcost.Vacuum*10)) str +=
+							'<input type="Button" onclick="Molpy.PantherRush(2)" value="Use Bonemeal"></input>';
+					}
 				}
 			}
 			return str;
@@ -6896,6 +6894,8 @@ Molpy.DefineBoosts = function() {
 		},
 		
 		defStuff: 1,
+		defSave: 1,
+		saveData: {4:['data',[]]},
 		
 		Add: function(amount) {
 			if(!this.data) this.data = [];
@@ -8127,7 +8127,7 @@ Molpy.DefineBoosts = function() {
 	}
 	Molpy.Hash = function(brown) {
 		var res = 0;
-		for (var c in brown.split('')) { res = res*2 + c.charCodeAt() };
+		for (var c in brown.split('')) { res = (((res<<1) + c.charCodeAt()) & 0x7FFFFFFF) + (res>>>16)};
 		return res;
 	}
 	Molpy.Decreename = '';
