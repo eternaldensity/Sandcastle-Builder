57212cc9692bca41412618083d492d0e239bcd1b
LucidCrux <galusha.m@gmail.com>
2014-03-10 15:50:17 -0600

    fixed long standing code error
    Molpy.Level('Panther Rush') was being assigned a value instead of
    Molpy.Boosts['Panter rush'].level, it was generating a code error.

--- boosts.js
+++ boosts.js
@@ -5306,7 +5306,7 @@ Molpy.DefineBoosts = function() {
 		},
 		defStuff: 1,
 
-		loadFunction: function() { if (Molpy.Earned('Einstein Says No')) Molpy.Level('Panther Rush') = 1079252850 *2; }
+		loadFunction: function() { if (Molpy.Earned('Einstein Says No')) Molpy.Boost['Panther Rush'].level = 1079252850 *2; }
 	});
 	
 	Molpy.Boosts['Panther Rush'].refreshFunction = undefined;
