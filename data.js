71aac4a1923e339a7ded9174b1486eb59a537894
Richard Proctor <richard@waveney.org>
2014-05-27 21:56:10 +0100

    Version 3.5105

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Molpy={};
-Molpy.version=3.5104;
+Molpy.version=3.5105;
 Molpy.versionName=''; // Appended to the numerical version on screen
 
 /**************************************************************
