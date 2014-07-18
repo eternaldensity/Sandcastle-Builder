e694d055dd1e8f5b460e77f4e3bf7e9f6b32e326
Richard Proctor <richard@waveney.org>
2014-07-18 09:56:19 +0100

    Yet another typo

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Molpy={};
-Molpy.version=3.6301;
+Molpy.version=3.6302;
 Molpy.versionName=''; // Appended to the numerical version on screen
 
 /**************************************************************
