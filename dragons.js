f70520f399613853a6ff7f1e40bd24c3c5a0fb7b
pickten <pickten@mathpathogens.org>
2015-08-13 17:39:56 -0400

    Major bugfix
    This may make it work

--- dragons.js
+++ dragons.js
@@ -398,7 +398,7 @@ Molpy.ClearNPdata = function() {
 	Molpy.NPdata = {};
 };
 Molpy.NextLegalNP=function(at){
-	if(Molpy.fracParts.indexOf(at)==Molpy.fracParts.length-1){
+	if(Molpy.fracParts.indexOf(at-Math.floor(at))==-1){
 		at=Math.floor(at)+1
 	} else{
 		at=Math.floor(at)+Molpy.fracParts[Molpy.fracParts.indexOf(at-Math.floor(at))+1]
