5971ab862a9073ea9c2cb8c9811dea3e9798c78e
pickten <pickten@mathpathogens.org>
2015-08-19 11:39:43 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1069,7 +1069,7 @@ Molpy.defineCrafts=function(){
 		return ans;
 	}
 	Molpy.craftID=function(ta,n,ti){
-		if(ti==undefined){ti=ta.times}
+		if(ti==undefined){ti=Molpy.Crafts[ta][n].times}
 		Molpy.craft(Molpy.Crafts[ta][n].recipe,ti)
 	}
 	Molpy.Crafts={
