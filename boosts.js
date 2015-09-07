22783cab2ac65f5cd81d6fca21481f3420704d2d
EPSIL0N <diablo_man0@hotmail.com>
2015-09-06 20:13:01 -0400

    Math sign hotfix to reflect +/- & -/+ time travel
    The previous code was treating time travel that flips over 0 the same as trying to time travel into 0.

--- boosts.js
+++ boosts.js
@@ -730,11 +730,11 @@ Molpy.DefineBoosts = function() {
 			Molpy.Notify('There is nothing before time.');
 			return;
 		}
-		if(Math.sign(np)!==Math.sign(oldnp) && !Molpy.Earned('Absolute Zero')) {
+		if(Math.sign(np)== 0 && Math.sign(oldnp)!== 0 && !Molpy.Earned('Absolute Zero')) {
 			Molpy.Notify('Divide by zero error!');
 			return;
 		}
-		if(Math.sign(np)!==Math.sign(oldnp) && Molpy.Earned('Absolute Zero')) {
+		if(Math.sign(np)== 0 && Math.sign(oldnp)!== 0 && Molpy.Earned('Absolute Zero')) {
 			Molpy.Notify('You cannot pass into NP0 directly;<br>charge your signpost.');
 			return;
 		}
