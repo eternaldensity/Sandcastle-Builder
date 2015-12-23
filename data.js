71abb9294a9735438f3a3f254d04f7c9f178a216
Hotdog Vendor <ed@chirpingmustard.com>
2015-12-24 07:18:16 +1100

    changelog and version number 4.001

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var Molpy={};
-Molpy.version=4.0;
-Molpy.versionName='Kitty Genocide<br>Simulator 2015'; // Appended to the numerical version on screen
+Molpy.version=4.001;
+Molpy.versionName='Kitty Genocide<br>Simulator 2015<br>Goating Intensifies'; // Appended to the numerical version on screen
 
 /**************************************************************
  * Game Strings
